7bbe4c15b962e6641fbba2105fce61cab7778a1c
MatNaw1998 <56668465+MatNaw1998@users.noreply.github.com>
2020-03-23 19:54:33 +0100

    Update filtr.m

--- filtr.m
+++ filtr.m
@@ -1,21 +1,21 @@
 function [filtred] = filtr(m,okno, prog)
 filtred = zeros(length(m(:,1)),length(m(1,:)));
 for i=1:(length(m(:,1))-okno+1)
-    for j=1:(length(m(1,:))-okno+1)
-        krok = 0;
-        for k=0:(okno-1)
-            if m(i+k,j+k)==1
-                krok = krok +1;
-            end
-            if krok >= (okno-prog)
-                for l=0:(okno-1)
-                    if m(i+l,j+l)==1
-                        filtred(i+l, j+l)=1;
-                    end
-                end
-            end
-        end
-    end
+for j=1:(length(m(1,:))-okno+1)
+zgodne = 0;
+for k=0:(okno-1)
+if m(i+k,j+k)==1
+zgodne = zgodne +1;
+end
+if zgodne >= (okno-prog)
+for l=0:(okno-1)
+if m(i+l,j+l)==1
+filtred(i+l, j+l)=1;
+end
+end
+end
+end
+end
 end
 spy(sparse(filtred));
 end
